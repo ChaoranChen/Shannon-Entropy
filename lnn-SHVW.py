7c0238ed5c7298f5f6f54909c9c05a81f9ff52a0
Chaoran Chen <907748200@qq.com>
2017-09-22 19:29:22 +0800

    Update lnn-SHVW.py

--- lnn-SHVW.py
+++ lnn-SHVW.py
@@ -1,15 +1,76 @@
-import numpy.random as nr
+import time
+import random
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from est_entro import est_entro_JVHW, est_entro_MLE
+import warnings
+warnings.filterwarnings('ignore')
+%matplotlib inline
 from math import log, pi, exp
 import lnn 
 
 def lnn_samplor(length):
     tmpList = np.array(ITM_samplor(length)[0]).reshape(length,1)
     return tmpList
-    
+
+def my_function(x):
+    if x<0 or x>1 :
+        return 0
+    elif x<1/4 or x>=3/4 :
+        return 0
+    elif x>=1/4 and x<1/2 :
+        return 16*(x-1/4)
+    elif x>=1/2 and x<3/4 :
+        return -16*(x-3/4)
+
+#Using acceptive-rejection method
+def sampler():
+    while True:
+        X = random.uniform(0.0, 1.0)
+        Y = random.uniform(0.0, 100.0)
+        if Y < my_function(X):
+            return X
+        
+def ARM_samplor(num, m=1):
+    numList = []
+    for _ in range(m):
+        tmpList = []
+        for i in range(num):
+            tmp = sampler()
+            tmpList.append(tmp)
+        numList.append(tmpList)
+        
+    return numList
+
+def itm(n=1000,c=0.5, a=0.25, h=4):
+    # invPosArray_ufunc1 = np.frompyfunc(invPosArray, 4, 1)
+    n = int(n)
+    yb = np.random.rand(1, n)
+    b = 2/h - a
+    if ((b + c) >1):
+        print("parameters out the defined domain")
+        return
+    # yb = np.array(yb)
+    x = ((yb <= 0.0)*(c-a)+
+    (yb > 0.0)*(yb < a*h/2)*((c-a) + (2*a*yb/h)**0.5)+
+    (yb >= a*h/2)*(yb < 1.0)*((c+b) -(2*b*(1-yb)/h)**0.5)+
+    (yb >= 1.0)*(c+b))
+    return x
+
+def ITM_samplor(num, m=1):
+    numList = []
+    for _ in range(m):
+        numList.append(itm(num).tolist()[0])
+        
+    return numList
+
+
 fig,ax = plt.subplots()
-length = [100]
-true = 0.5*(1-4*np.log(2))/np.log(2)
 m = 100
+length = [m]
+true = 0.5*(1-4*np.log(2))/np.log(2)
 errorSet = []
 
 for _ in range(m):
@@ -18,35 +79,21 @@ for _ in range(m):
         H = lnn.entropy(data)
         err = np.sqrt(np.mean(np.square(H-true)))
         errorSet.append(err)
-        #print ('data size =', l)
-        #print ("Ground Truth = ", true)
-        #print ("LNN: H(X) =  ", H)
-        #print ("RMSE = ", err)
         
-ax.plot(range(m), errorSet, 's-', linewidth=1.5)
-ax.set_title('n = 10000, m = 100, lnn.entropy')
 
-mc_times = 50 # Total number of Monte-Carlo trials for each alphabet size
-#record_n = np.array([100000, 10000, 1000, 100]) # sample size 
+mc_times = 50
 record_n = np.array([100])
 record_h = [1/3]
 num = len(record_h)
-
 
 JVHW_err = np.zeros(num)
 total_time = []
 err = 0
 errset1 = []
-#fig,ax = plt.subplots()
 for order in range(m):
     for n in record_n:
-        start = time.clock()
         #tmpSamplor = ARM_samplor(n, m = mc_times) # ARM sampling is much slower than ITM
         tmpSamplor = np.array(ITM_samplor(n, m = mc_times)).reshape(n,1)
-        elapsed = (time.clock() - start)
-        #print('Sampling of n='+str(n)+' has finished. Time use:',elapsed)
-        total_time.append(elapsed)
-        start = time.clock()
         for i, h in enumerate(record_h):
             S = int(1/h)
             edges = np.linspace(0,1,S+1)
@@ -57,21 +104,7 @@ for order in range(m):
             if (h == 1/3):
                 err = JVHW_err[i]
 
-        elapsed = (time.clock() - start)
-        #print('Calculation of n='+str(n)+' has finished. Time use:',elapsed)
-        total_time.append(elapsed)
-        
-        
-        start = time.clock()
-        #print(n,' ',JVHW_err)
-        #ax.plot(record_h, JVHW_err, 's-', linewidth=1.5)
-        #ax.plot(order, JVHW_err, 's-', linewidth=1.5)
-        elapsed = (time.clock() - start)
-        #print('Plotting of n='+str(n)+' has finished. Time use:',elapsed)
-        total_time.append(elapsed)
 errset1 = np.array(errset1).reshape(100,1)
-#ax.plot(range(100), errset1, 's-', linewidth=1.5)
-#ax.set_title('n = 1000, m = 100, SHVW.entropy')
 
 fig,ax = plt.subplots()
 ax.plot(range(m), errorSet, 's-', linewidth=1.5)
